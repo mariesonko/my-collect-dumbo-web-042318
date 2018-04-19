b337afe82c327ef474572b14853b992fdb1451b9
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 15:31:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+    n=0
+    my_collection =[]
+    while n < collection.length
+    my_collection << (yield collection[n])
+      n+=1
+    end
+    my_collection
+  end
