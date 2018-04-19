ad6eeef53cf6d51c2108c87a720d43f4e442f214
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 15:27:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if block_given?
+    n=0
+    collection =[]
+    while n <array.length
+    collection << yield array[n]
+      n+=1
+    end
+    collection 
+  end
+end
